8c927e9880def3f748ba1c90fd124a17ae85f23e
Arnav <arnav.aggarwal@thecontrolgroup.com>
2017-06-09 16:03:33 -0700

    Configure eslint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es6': true,
+        'node': true,
+    },
+    'parser': 'babel-eslint',
+    'plugins': ['react', 'jsx-a11y', 'import'],
+    'rules': {
+        'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx'] }],
+        'react/no-unescaped-entities': [0],
+        'react/no-array-index-key': [0],
+        'react/prop-types': [0],
+        'no-mixed-operators': [0],
+        'indent': ['error', 4],
+        'react/jsx-indent': [2, 4],
+        'max-len': [0], 
+    },
+    'extends': 'airbnb',
+};
