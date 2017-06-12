33502a40d62d1aac204088c6a4fa9b1a14e44658
Arnav <arnav.aggarwal@thecontrolgroup.com>
2017-06-12 10:44:25 -0700

    Merge changes from master

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
@@ -7,9 +5,15 @@ const port = process.env.PORT || 8080;
 app.set('port', port);
 app.use(express.static(__dirname + '/dist'));
 
+// app.get('*.gz', (req, res, next) => {
+//     console.log('Serving .gz');
+//     res.set('Content-Encoding', 'gzip');
+//     next();
+// });
+
 app.get('*', (req, res) => {
-    console.log('Serving /');
+    console.log('Serving ', req.url);
     res.sendFile(__dirname + '/dist/app.html');
 });
 
-const server = app.listen(port, () => console.log('Listening on port', port));
+app.listen(port, () => console.log('Listening on port', port));
