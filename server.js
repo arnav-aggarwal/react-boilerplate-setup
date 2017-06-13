6a81771954d64ea862296d1ac6f964e0eec9e413
Arnav <arnav.aggarwal@thecontrolgroup.com>
2017-06-13 10:46:51 -0700

    Clean up server.js - remove unneeded comments Clean up package.json - remove compression-plugin

--- server.js
+++ server.js
@@ -5,12 +5,6 @@ const port = process.env.PORT || 8080;
 app.set('port', port);
 app.use(express.static(__dirname + '/dist'));
 
-// app.get('*.gz', (req, res, next) => {
-//     console.log('Serving .gz');
-//     res.set('Content-Encoding', 'gzip');
-//     next();
-// });
-
 app.get('*', (req, res) => {
     console.log('Serving ', req.url);
     res.sendFile(__dirname + '/dist/app.html');
